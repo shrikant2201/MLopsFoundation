bdc1c4e14b3831508037618a611c4fe2b8663444
shrikant2201 <shrikant2201@gmail.com>
2024-07-27 22:17:15 +0530

    requirement file

--- src/train.py
+++ src/train.py
@@ -8,7 +8,12 @@ X = np.random.rand(100, 1)
 y = 2 * X + np.random.randn(100, 1)
 
 # Split the data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
 
 # Train a model
 model = LinearRegression()
